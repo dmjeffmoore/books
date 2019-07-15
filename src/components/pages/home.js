37edfcbdd9a340c2860691305ff7114056b9ce97
Jeffrey Moore <dmjeffmoore@gmail.com>
2019-07-14 22:57:49 -0700

    Added server.js and primereact ui framework

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { TabMenu } from 'primereact/tabmenu';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tabItems: [
+                {label: 'Browse', icon: 'pi pi-fw pi-info-circle'},
+                {label: 'Search', icon: 'pi pi-fw pi-search'}
+            ],
+            activeTab: {}
+        };
+    }
+
     render() {
         return (
             <div>
-                test
+                <TabMenu model={this.state.tabItems} activeItem={this.state.activeTab} onTabChange={(e) => this.setState({activeTab: e.value})}/>
+
+
             </div>
         );
     }
