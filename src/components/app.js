f6bdae8514b33d6fc422eacdc74fe350be37953b
Jeffrey Moore <dmjeffmoore@gmail.com>
2019-10-15 20:58:11 -0700

    Added user management functionality with Okta

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
 
 import Home from './pages/home';
 import NotFound from './pages/notFound';
 import Menu from './menu';
 
+const config = {
+    issuer: process.env.ISSUER,
+    redirectUri: window.location.origin + '/implicit/callback',
+    clientId: process.env.CLIENT_ID,
+    pkce: true
+};
+
 function App() {
     return(
         <Router>
-            <Menu />
-            <div id="content">
                 <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="*" component={NotFound}/>
+                    <Security {...config}>
+                        <Menu />
+                        <Route exact path="/" component={Home} />
+                        <Route path="/implicit/callback" component={ImplicitCallback}/>
+                    </Security>
                 </Switch>
-            </div>
         </Router>
     )
 }
